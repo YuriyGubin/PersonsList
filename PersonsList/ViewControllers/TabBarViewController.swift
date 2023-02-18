0fc966f795d35992e74d131ab5e6d206a4f9ecc4
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-18 21:22:26 +0500

    Final variant

--- PersonsList/ViewControllers/TabBarViewController.swift
+++ PersonsList/ViewControllers/TabBarViewController.swift
@@ -14,6 +14,7 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabBar()
+        dataTransfer()
     }
 
     private func setupTabBar() {
